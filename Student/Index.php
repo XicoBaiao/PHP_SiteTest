a8bd3fee9afc277c85b91ca3ad12a004f67d6ff3
XicoBaiao <baiao_maxima@outlook.com>
2017-07-07 02:04:27 +0100

    Added Student Folder

---
+++ Student/Index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link href="/assets/styles.css" rel="stylesheet">
+</head>
+<body id="final-example">
+    <div class="wrapper">
+        <div id="banner">
+            <a href="/" title="Return to Home"></a>
+                <img src="img/baiao.jpg" alt="Vida do Baião">
+        </div><!-- banner -->
+
+        <div id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="SobreMim.php">Sobre Mim</a></li>
+                <li><a href="Qualificacoes.php">Qualificações</a></li>
+                <li><a href="Contactos.php">Contactos</a></li>
+            </ul>
+
+        </div><!-- nav -->
+
+        <div class="content">
+            <div id="footer" class="cf">
+                <div class="column three">
+                    <strong>Telemóvel</strong>
+                    932367457
+                </div><!-- column -->
+                <div class="column three">
+                    <strong>Localização</strong>
+                    Portimão, Faro<br>
+                    Portugal
+                </div><!-- column -->
+                <div class="column three last">
+                    <strong>Horas Disponíveis</strong>
+                    <em> Segunda - Domingo</em><br>
+                    00:00-23:59 <br><br>
+                </div><!-- column -->
+
+            </div><!-- footer -->
+            <small>&copy;2017 Francisco Baião</small>
+        </div><!-- content -->
+
+    </div><!-- wrapper -->
+
+    <div class="copyright-info">
+        <?php include('../assets/includes/copyright.php'); ?>
+    </div><!-- copyright-info -->
+</body>
+</html>
